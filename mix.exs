9b6ce9dd97905d7c91385cf3ab4759a1c0a66140
Long Nguyen <long@acuity.sg>
2023-06-22 22:57:01 +0700

    Upgrade HTTPoinsion

--- mix.exs
+++ mix.exs
@@ -2,27 +2,26 @@ defmodule Exometer.NewrelicReporter.Mixfile do
   use Mix.Project
 
   def project do
-    [
-      app: :exometer_newrelic_reporter,
-      version: "0.1.0",
-      elixir: "~> 1.4",
-      build_embedded: Mix.env() == :prod,
-      start_permanent: Mix.env() == :prod,
-      test_coverage: [tool: ExCoveralls],
-      deps: deps()
-    ]
+    [app: :exometer_newrelic_reporter,
+     version: "0.1.0",
+     elixir: "~> 1.4",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+     deps: deps()]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:httpoison, :logger], mod: {Exometer.NewrelicReporter, []}]
+    [applications: [:httpoison, :logger],
+     mod: {Exometer.NewrelicReporter, []}]
   end
 
   defp deps do
     [
-      {:httpoison, "~> 0.9.0"},
+      {:httpoison, "~> 1.8"},
       {:poison, "~> 2.0"},
       {:excoveralls, "~> 0.6", only: :test}
     ]
